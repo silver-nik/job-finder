81d3bf100b8c3cad49cb8bf7a4ed5ea842d469c5
Shvetsov Nikita <send0@yandex.ru>
2024-05-05 18:27:25 +1000

    added init structure role-based access

--- src/components/animatedRoutes/animatedRoutes.js
+++ src/components/animatedRoutes/animatedRoutes.js
@@ -1,5 +1,5 @@
 import {lazy} from 'react';
-import {Route, Routes, useLocation} from 'react-router-dom';
+import {Route, Routes, useLocation, Outlet, Navigate} from 'react-router-dom';
 // import { AnimatePresence } from 'framer-motion';
 
 // const Page404 = lazy(() => import('../pages/404'));
@@ -7,6 +7,8 @@ const MainPage = lazy(() => import('../pages/mainPage/mainPage'));
 const SearchPage = lazy(() => import('../pages/searchPage/searchPage'));
 const BlogPage = lazy(() => import('../pages/blogPage/blogPage'));
 const SingleBlogPage = lazy(() => import('../pages/singleBlogPage/singleBlogPage'));
+const LoginPage = lazy(() => import('../pages/loginPage/loginPage'));
+
 
 
 
@@ -19,17 +21,31 @@ const SingleBlogPage = lazy(() => import('../pages/singleBlogPage/singleBlogPage
 
 const AnimatedRoutes = () => {
     const location = useLocation();
+
+    const RoleAccess = ({ roles = [] }) => {
+
+        const user = 'user';
+        // const user = JSON.parse(localStorage.getItem("userInfo"));
+        // return !roles.length || roles.includes(user?.role)
+        return !roles.length || roles.includes(user)
+          ? <Outlet />
+          : 'You havent permisssions';
+    };
   
     return (
     //   <AnimatePresence mode="wait">
         <Routes key={location.pathname} location={location} >
 
             <Route path="/" element={<MainPage/>} className='page'/>
-            {/* {/* <Route path="/comics" element={<ComicsPage/>} className='page' /> */}
+            <Route path="/login" element={<LoginPage/>} className='page' />
+
+            <Route element={<RoleAccess roles={["user", "admin"]} />}>
+                <Route path="/blog" element={<BlogPage />} />
+            </Route>
             
             {/* <Route path="/search/vacancy/:text/:area" element={<SearchPage/>} className='page' /> */}
             <Route path="/blog/:id" element={<SingleBlogPage/>} className='page' />
-            <Route path="/blog" element={<BlogPage/>} className='page' />
+            {/* <Route path="/blog" element={<BlogPage/>} className='page' /> */}
             <Route path="/search/vacancy" element={<SearchPage/>} className='page' />
             {/* <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>}/>
             <Route path="*" element={<Page404/>}/>
